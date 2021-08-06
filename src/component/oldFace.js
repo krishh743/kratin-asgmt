40a8d1d0fd8b4723acc133d8fe63a262b3d6b4c1
Krishna kushwaha <63386697+krishh743@users.noreply.github.com>
2021-08-06 14:43:15 +0530

    third commit

--- src/component/oldFace.js
+++ src/component/oldFace.js
@@ -1,7 +1,7 @@
 import React from "react";
-// add css to style the facebox
+
 import "./oldFace.css";
-// pass the box state to the component
+
 
 const oldFace = ({ imageUrl, box }) => {
   return (
@@ -9,16 +9,16 @@ const oldFace = ({ imageUrl, box }) => {
       <div className="absolute mt2">
             /* insert an id to be able to manipulate the image in the DOM */
         <img id="inputimage" alt="" src={imageUrl} width="500px" heigh="auto" />
-       //this is the div displaying the faceDetect box base on the bounding box value 
+       
       <div
           className="bounding-box"
-          // styling that makes the box visible base on the return value
-          // style={{
-          //   top: box.topRow,
-          //   right: box.rightCol,
-          //   bottom: box.bottomRow,
-          //   left: box.leftCol,
-          // }}
+          styling that makes the box visible base on the return value
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
         ></div>
       </div>
     </div>
