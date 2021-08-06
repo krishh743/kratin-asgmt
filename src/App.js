03f8156c167e66384f0aa58f35505acb08b01ead
Krishna kushwaha <63386697+krishh743@users.noreply.github.com>
2021-08-06 14:46:05 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ class App extends Component {
   };
 
   onSubmit = () => {
+    
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
